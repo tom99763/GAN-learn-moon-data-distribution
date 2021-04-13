255c3870c5754372594d6a093ca9fb7788291785
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-04-13 18:56:52 +0800

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -6,6 +6,8 @@ class discriminator_loss(nn.Module):
         super(discriminator_loss, self).__init__()
 
     def forward(self,p_data,p_g):
+        #minimize entropy of D(x)---> maximize D(x)
+        #minimize entropy of 1-D(G(z)) ----> minimize D(G(z))
         term_1=torch.log(p_data)
         term_2=torch.log(1-p_g)
         loss=-torch.mean(term_1+term_2)
@@ -17,6 +19,6 @@ class generator_loss(nn.Module):
         super(generator_loss, self).__init__()
 
     def forward(self,p_g):
-        #minimize entropy of 1-D(G(z)) distribution
+        #minimize negative entropy of 1-D(G(z)) --->maximize D(G(z))
         loss=torch.mean(torch.log(1-p_g))
         return loss
